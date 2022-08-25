94d6456504a62a499295668239f20f63517f1e00
Megan <meganmoulos@gmail.com>
2022-08-25 12:53:01 -0700

    commit tree ecc2ace1315f1431a503664c8011cd07f6880657

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+class Polygon {
+    constructor(sides) {
+        this.sides = sides;
+    }
+
+    get countSides() {
+        let count = 0
+        this.sides.forEach(element => {
+            count++
+        })
+        return count
+    }
+
+    get perimeter() {
+        const initialValue = 0
+        const sumWithInitial = this.sides.reduce(
+            (previousValue, currentValue) => previousValue + currentValue,
+            initialValue
+          )
+          return(sumWithInitial)
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        if(this.countSides !== 3) {
+            return false
+        } else {
+            return true
+        }
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        if(this.sides[0] === this.sides[1]){
+            return true
+        }
+    }
+}
